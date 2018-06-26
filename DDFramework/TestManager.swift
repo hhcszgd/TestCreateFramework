16636cda8ae01211bf2142452b7f5da7d5e3baff
wangyuanfei <979433528@qq.com>
2018-06-26 20:49:28 +0800

    add framework and testmanager class ,then test use TestManager in project

---
+++ DDFramework/TestManager.swift
@@ -0,0 +1,15 @@
+//
+//  TestManager.swift
+//  DDFramework
+//
+//  Created by WY on 2018/6/26.
+//  Copyright © 2018年 HHCSZGD. All rights reserved.
+//
+
+import UIKit
+
+open class TestManager: NSObject {
+    open static func printSomething(){
+        print("success !!!")
+    }
+}
